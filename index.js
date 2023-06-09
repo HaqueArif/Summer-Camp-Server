69ede7f326bfdc2b4b34a7ba0b952c16ee12d383
HaqueArif <haquearif143@gmail.com>
2023-06-09 15:10:01 +0600

    created post api for users data

--- index.js
+++ index.js
@@ -9,6 +9,21 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// const verifyJWT = (req, res, next) => {
+//   const authorization = req.headers.authorization;
+//   if(!authorization){
+//     return res.status(401).send({ error: true, message: 'unauthorized access'})
+//   };
+//   const token = authorization.split(' ')[1];
+//   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) =>{
+//     if(err){
+//       return res.status(401).send({error: true, message: 'unauthorized access'})
+//     }
+//     req.decoded = decoded;
+//     next();
+//   })
+// }
+
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bihlgkr.mongodb.net/?retryWrites=true&w=majority`;
@@ -32,21 +47,14 @@ async function run() {
     const dataCollection = client.db('summerCampDb').collection('data');
     const classCollection = client.db('summerCampDb').collection('classes');
 
-    const verifyJWT = (req, res, next) => {
-      const authorization = req.headers.authorization;
-      if(!authorization){
-        return res.status(401).send({ error: true, message: 'unauthorized access'})
-      };
-      const token = authorization.split(' ')[1];
-      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) =>{
-        if(err){
-          return res.status(401).send({error: true, message: 'unauthorized access'})
-        }
-        req.decoded = decoded;
-        next();
+    
+    app.post('/jwt', (req, res)=>{
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: '1h'
       })
-    }
-
+      res.send({token})
+    })
 
     app.get('/users', async(req, res)=>{
       const result = await usersCollection.find().toArray();
