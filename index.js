eb396128130d180066e1e0ac94875760feb4ad62
HaqueArif <haquearif143@gmail.com>
2023-06-09 15:43:13 +0600

    added jwt

--- index.js
+++ index.js
@@ -72,6 +72,19 @@ async function run() {
       res.send(result)
     })
 
+    app.get('/users/admin/:email', verifyJWT, async(req, res) =>{
+      const email = req.params.email;
+
+      if(req.decoded.email !== email){
+        res.send({admin:false})
+      }
+      
+      const query = {email: email}
+      const user = await usersCollection.findOne(query)
+      const result = {admin: user?.role === 'admin'}
+      res.send(result)
+    })
+
     app.patch('/users/admin/:id', async(req, res)=>{
       const id = req.params.id;
       const filter = {_id: new ObjectId(id)}
