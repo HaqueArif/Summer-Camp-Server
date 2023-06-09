91968456cdbe4829b3f04209b635bba6cb6806e9
HaqueArif <haquearif143@gmail.com>
2023-06-09 15:20:40 +0600

    created get api for users data

--- index.js
+++ index.js
@@ -9,20 +9,20 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// const verifyJWT = (req, res, next) => {
-//   const authorization = req.headers.authorization;
-//   if(!authorization){
-//     return res.status(401).send({ error: true, message: 'unauthorized access'})
-//   };
-//   const token = authorization.split(' ')[1];
-//   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) =>{
-//     if(err){
-//       return res.status(401).send({error: true, message: 'unauthorized access'})
-//     }
-//     req.decoded = decoded;
-//     next();
-//   })
-// }
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if(!authorization){
+    return res.status(401).send({ error: true, message: 'unauthorized access'})
+  };
+  const token = authorization.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) =>{
+    if(err){
+      return res.status(401).send({error: true, message: 'unauthorized access'})
+    }
+    req.decoded = decoded;
+    next();
+  })
+}
 
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -104,12 +104,18 @@ async function run() {
         res.send(result)
     })
 
-    app.get('/classes', async (req, res)=>{
+    app.get('/classes', verifyJWT, async (req, res)=>{
       const email = req.query.email;
       console.log(email);
       if(!email){
         res.send([]);
       }
+
+      const decodedEmail = req.decoded.email;
+      if(email !== decodedEmail){
+        return res.status(403).send({error: true, message: 'forbidden access'})
+      }
+      
       const query = {email: email};
       const result = await classCollection.find(query).toArray();
       res.send(result)
